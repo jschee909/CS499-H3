c7ff3c9b1e3f4562badcf2838d7ed22475c02f97
jschee909 <james.chee000@gmail.com>
2017-03-08 10:58:32 -0800

    flask and ui up

--- traffic.py
+++ traffic.py
@@ -3,6 +3,7 @@
 import numpy as np
 from sklearn import preprocessing
 from sklearn.svm import SVR
+from flask import Flask, render_template, request
 import matplotlib.pyplot as plt 
 
 input_file = 'traffic_data.txt'
@@ -14,19 +15,25 @@ count = 0
 with open(input_file, 'r') as f:
     for line in f.readlines():
         data = line[:-1].split(',')
-        X.append(data[:1])
+        X.append(data[:])
         Y.append(data[-1])
 
 Y[-1] = 9
 print(X)
 print(Y)
 
-colors = colors = ['black', 'blue', 'purple', 'yellow', 'white', 'red', 'lime', 'cyan', 'orange', 'gray']
+app = Flask(__name__)
 
-for i in range(len(colors)):
-    plt.scatter(X, Y, c=colors[i])
-    
-plt.xlabel('First Principal Component')
-plt.ylabel('Second Principal Component')
-plt.title("PCA Scatter Plot")
-plt.show()
+@app.route('/')
+def index():
+  return render_template('template.html')
+
+@app.route('/traffic.py')
+def data():
+  print 'Data Submitted'
+  req = request.args.get('wanted')
+
+  return 'Thank You.'
+
+if __name__ == '__main__':
+  app.run(debug=True)
